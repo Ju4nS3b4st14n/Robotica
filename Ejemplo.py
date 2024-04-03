a8f093b45e1b8040630e6d76976cc1d246b8e1c2
Ju4nS3b4st14n <126536935+Ju4nS3b4st14n@users.noreply.github.com>
2024-04-02 21:25:44 -0500

    Update Ejemplo.py

--- Ejemplo.py
+++ Ejemplo.py
@@ -1,17 +1,68 @@
-from gpiozero import DistanceSensor
+from PyQt5 import QtCore, QtGui, QtWidgets
+from gpiozero import *
 import time
 
-# Configurar el sensor de ultrasonido en los pines GPIO específicos
-sensor = DistanceSensor(echo=18, trigger=17)
 
-try:
-    while True:
-        # Leer la distancia medida por el sensor
-        distancia = sensor.distance
-        print(f"Distancia: {distancia} metros")
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(250, 60, 301, 17))
+        self.label.setObjectName("label")
+        self.textEdit = QtWidgets.QTextEdit(self.centralwidget)
+        self.textEdit.setGeometry(QtCore.QRect(330, 110, 113, 25))
+        self.textEdit.setObjectName("textEdit")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(340, 160, 89, 25))
+        self.pushButton.setObjectName("pushButton")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(320, 220, 131, 17))
+        self.label_2.setObjectName("label_2")
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(320, 270, 145, 17))
+        self.label_3.setObjectName("label_3")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
 
-        # Esperar un breve tiempo antes de tomar otra lectura
-        time.sleep(1)
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-except KeyboardInterrupt:
-    print("Lectura del sensor detenida por el usuario")
+        self.pushButton.clicked.connect(self.tiempo)
+
+        self.sensor = DistanceSensor(echo=18, trigger=17)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label.setText(_translate("MainWindow", "Tiempo de muestra del sensor en segundos"))
+        self.pushButton.setText(_translate("MainWindow", "Iniciar"))
+        self.label_2.setText(_translate("MainWindow", "Lextura del sensor"))
+        self.label_3.setText(_translate("MainWindow", "Distancia ... metros"))
+
+    def tiempo(self):
+
+        t = self.textEdit.toPlainText()
+        t1 = time.time()
+        t2 = 0.0
+        while t2 <= float(t):
+            distancia = self.sensor.distance
+            self.label_3.setText("Distancia {:.2f} metros".format(distancia))
+            QtWidgets.QApplication.processEvents()
+            print(f"El tiempo es {t2}s \n")
+            t2 = time.time() - t1
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
