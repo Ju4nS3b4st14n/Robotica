85f4a0ee76d894810bee1fc822f290f16d7dabbc
Ju4nS3b4st14n <126536935+Ju4nS3b4st14n@users.noreply.github.com>
2024-04-30 11:37:16 -0500

    Update Ejemplo.py

--- Ejemplo.py
+++ Ejemplo.py
@@ -1,18 +1,45 @@
-# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
-# SPDX-License-Identifier: MIT
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-"""Simple test for a standard servo on channel 0 and a continuous rotation servo on channel 1."""
-import time
-from adafruit_servokit import ServoKit
+# sudo apt-get install -y python-smbus
+# sudo apt-get install -y i2c-tools
+# sudo i2cdetect -y 1
 
-# Set channels to the number of servo channels on your kit.
-# 8 for FeatherWing, 16 for Shield/HAT/Bonnet.
-kit = ServoKit(channels=8)
+# sudo pip3 install adafruit-circuitpython-servokit
 
-kit.servo[0].angle = 180
-kit.continuous_servo[1].throttle = 1
-time.sleep(1)
-kit.continuous_servo[1].throttle = -1
-time.sleep(1)
-kit.servo[0].angle = 0
-kit.continuous_servo[1].throttle = 0
+#Libraries
+import time    
+from adafruit_servokit import ServoKit   
+#Constants
+nbPCAServo=16 
+#Parameters
+MIN_IMP  =[500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500]
+MAX_IMP  =[2500, 2500, 2500, 2500, 2500, 2500, 2500, 2500, 2500, 2500, 2500, 2500, 2500, 2500, 2500, 2500]
+MIN_ANG  =[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+MAX_ANG  =[180, 180, 180, 180, 180, 180, 180, 180, 180, 180, 180, 180, 180, 180, 180, 180]
+#Objects
+pca = ServoKit(channels=16)
+# function init 
+def init():
+    for i in range(nbPCAServo):
+        pca.servo[i].set_pulse_width_range(MIN_IMP[i] , MAX_IMP[i])
+# function main 
+def main():
+    pcaScenario();
+# function pcaScenario 
+def pcaScenario():
+    """Scenario to test servo"""
+    for i in range(nbPCAServo):
+        for j in range(MIN_ANG[i],MAX_ANG[i],1):
+            print("Send angle {} to Servo {}".format(j,i))
+            pca.servo[i].angle = j
+            time.sleep(0.01)
+        for j in range(MAX_ANG[i],MIN_ANG[i],-1):
+            print("Send angle {} to Servo {}".format(j,i))
+            pca.servo[i].angle = j
+            time.sleep(0.01)
+        pca.servo[i].angle=None #disable channel
+        time.sleep(0.5)
+if __name__ == '__main__':
+    init()
+    main()
