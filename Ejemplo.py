e4a52508e6fe40dcb0a5599fd8294632c90e0fc4
Ju4nS3b4st14n <juans.torresr@ecci.edu.co>
2024-05-07 14:49:37 -0500

    update

--- Ejemplo.py
+++ Ejemplo.py
@@ -1,152 +1,42 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
 from roboticstoolbox import *
-import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.figure import Figure
-import RPi.GPIO as GPIO
-from time import sleep
+from spatialmath.base import *
+import math
+import numpy
 
+l1 = 10
+l2 = 10
 
-class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(769, 837)
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
-        self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(180, 40, 21, 25))
-        self.label.setObjectName("label")
-        self.label_2 = QtWidgets.QLabel(self.centralwidget)
-        self.label_2.setGeometry(QtCore.QRect(180, 70, 21, 25))
-        self.label_2.setObjectName("label_2")
-        self.textEdit = QtWidgets.QTextEdit(self.centralwidget)
-        self.textEdit.setGeometry(QtCore.QRect(220, 40, 104, 25))
-        self.textEdit.setObjectName("textEdit")
-        self.textEdit_2 = QtWidgets.QTextEdit(self.centralwidget)
-        self.textEdit_2.setGeometry(QtCore.QRect(220, 70, 104, 25))
-        self.textEdit_2.setObjectName("textEdit_2")
-        self.label_7 = QtWidgets.QWidget(self.centralwidget)
-        self.label_7.setGeometry(QtCore.QRect(100, 140, 551, 371))
-        self.label_7.setObjectName("label_7")
-        self.label_3 = QtWidgets.QLabel(self.centralwidget)
-        self.label_3.setGeometry(QtCore.QRect(396, 40, 101, 20))
-        self.label_3.setObjectName("label_3")
-        self.label_4 = QtWidgets.QLabel(self.centralwidget)
-        self.label_4.setGeometry(QtCore.QRect(396, 70, 101, 20))
-        self.label_4.setObjectName("label_4")
-        self.label_5 = QtWidgets.QLabel(self.centralwidget)
-        self.label_5.setGeometry(QtCore.QRect(510, 70, 67, 17))
-        self.label_5.setText("")
-        self.label_5.setObjectName("label_5")
-        self.label_6 = QtWidgets.QLabel(self.centralwidget)
-        self.label_6.setGeometry(QtCore.QRect(510, 40, 67, 17))
-        self.label_6.setText("")
-        self.label_6.setObjectName("label_6")
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
+# Cinemática inversa
+Px = -7
+Py = -5
 
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+b = math.sqrt(Px**2+Py**2)
+# Theta 2
+cos_theta2 = (b**2-l2**2-l1**2)/(2*l1*l2)
+sen_theta2 = math.sqrt(1-(cos_theta2)**2)#(+)codo abajo y (-)codo arriba
+theta2 = math.atan2(sen_theta2, cos_theta2)
+print(f'theta 2 = {numpy.rad2deg(theta2):.4f}')
+# Theta 1
+alpha = math.atan2(Py,Px)
+phi = math.atan2(l2*sen_theta2, l1+l2*cos_theta2)
+theta1 = alpha - phi
+print(f'theta 1 = {numpy.rad2deg(theta1):.4f}')
+#-------------
 
-        self.textEdit.textChanged.connect(self.robot)
-        self.textEdit_2.textChanged.connect(self.robot)
+q1 = theta1
+q2 = theta2
 
-        self.selected_motor = None
+R = []
+R.append(RevoluteDH(d=0, alpha=0, a=l1, offset=0))
+R.append(RevoluteDH(d=0, alpha=0, a=l2, offset=0))
 
-    def retranslateUi(self, MainWindow):
-        _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.label.setText(_translate("MainWindow", "X"))
-        self.label_2.setText(_translate("MainWindow", "Y"))
-        self.label_3.setText(_translate("MainWindow", "Articulación 1"))
-        self.label_4.setText(_translate("MainWindow", "Articulación 2"))
+Robot = DHRobot(R, name='Bender')
+print(Robot)
 
-    def robot(self):
-        l1 = 6
-        l2 = 8
-        # Cinemática inversa
-        x = self.textEdit.toPlainText()
-        y = self.textEdit_2.toPlainText()
-        Px = int(x)
-        Py = int(y)
+Robot.teach([q1, q2], 'rpy/zyx', limits=[-30,30,-30,30,-30,30])
 
-        b = np.sqrt(Px ** 2 + Py ** 2)
-        # Theta 2
-        cos_theta2 = (b ** 2 - l2 ** 2 - l1 ** 2) / (2 * l1 * l2)
-        sen_theta2 = np.sqrt(1 - (cos_theta2) ** 2)
-        theta2 = np.arctan2(sen_theta2, cos_theta2)
+#zlim([-15,30]);
 
-        # Theta 1
-        alpha = np.arctan2(Py, Px)
-        phi = np.arctan2(l2 * sen_theta2, l1 + l2 * cos_theta2)
-        theta1 = alpha - phi
-
-        q1 = theta1
-        self.label_6.setText(str(np.rad2deg(q1)))
-        q2 = theta2
-        self.label_5.setText(str(np.rad2deg(q2)))
-
-        R = []
-        R.append(RevoluteDH(d=0, alpha=0, a=l1, offset=0))
-        R.append(RevoluteDH(d=0, alpha=0, a=l2, offset=0))
-
-        Robot = DHRobot(R, name='Bender')
-
-        # self.selected_motor = 33
-        # self.selected_motor = 35
-
-        self.plot_robot(Robot, q1, q2)
-
-    def plot_robot(self, robot, q1, q2):
-
-        fig = Figure()
-        ax = fig.add_subplot(111, projection='3d')
-        robot.plot([q1, q2], backend='pyplot', limits=[-20, 20, -20, 20, -20, 20])
-        ax.set_xlim([-20, 20])
-        ax.set_ylim([-20, 20])
-        ax.set_zlim([-20, 20])
-
-        canvas = FigureCanvas(fig)
-        layout = QtWidgets.QVBoxLayout(self.label_7)
-        layout.addWidget(canvas)
-
-        self.move_robot(q1, q2)
-
-    def move_robot(self, q1, q2):
-
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(33, GPIO.OUT)
-        GPIO.setup(35, GPIO.OUT)
-        pulso_q1 = GPIO.PWM(33, 50)
-        pulso_q2 = GPIO.PWM(35, 50)
-        pulso_q1.start(1.5)
-        pulso_q2.start(1.5)
-
-        grados_q1 = ((1.0/18.0) * q1) + 2.5 
-        pulso_q1.ChangeDutyCycle(grados_q1)
-        sleep(0.01)
-        pulso_q1.stop()
-        GPIO.cleanup()
-
-        grados_q2 = ((1.0/18.0) * q2) + 2.5 
-        pulso_q2.ChangeDutyCycle(grados_q2)
-        sleep(0.01)
-        pulso_q2.stop()
-        GPIO.cleanup()
-
-
-if __name__ == "__main__":
-    import sys
-
-    app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
-    sys.exit(app.exec_())
-
-
+MTH = Robot.fkine([q1,q2])
+print(MTH)
+print(f'Roll, Pitch, Yaw = {tr2rpy(MTH.R, "deg", "zyx")}')
--- Laboratorio_3/Punto1.py
+++ Laboratorio_3/Punto1.py
@@ -1,36 +1,34 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import QTimer
 from roboticstoolbox import *
 import numpy as np
+import math
 import matplotlib
 matplotlib.use('Qt5Agg')
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
-import RPi.GPIO as GPIO
 from time import sleep
-
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(769, 837)
+        MainWindow.resize(769, 750)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(180, 40, 21, 25))
-        self.label.setObjectName("label")
-        self.label_2 = QtWidgets.QLabel(self.centralwidget)
-        self.label_2.setGeometry(QtCore.QRect(180, 70, 21, 25))
-        self.label_2.setObjectName("label_2")
         self.textEdit = QtWidgets.QTextEdit(self.centralwidget)
         self.textEdit.setGeometry(QtCore.QRect(220, 40, 104, 25))
         self.textEdit.setObjectName("textEdit")
         self.textEdit_2 = QtWidgets.QTextEdit(self.centralwidget)
         self.textEdit_2.setGeometry(QtCore.QRect(220, 70, 104, 25))
         self.textEdit_2.setObjectName("textEdit_2")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(180, 40, 21, 25))
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(180, 70, 21, 25))
+        self.label_2.setObjectName("label_2")
         self.label_7 = QtWidgets.QWidget(self.centralwidget)
-        self.label_7.setGeometry(QtCore.QRect(100, 140, 551, 371))
+        self.label_7.setGeometry(QtCore.QRect(100, 100, 551, 371))
         self.label_7.setObjectName("label_7")
         self.label_3 = QtWidgets.QLabel(self.centralwidget)
         self.label_3.setGeometry(QtCore.QRect(396, 40, 101, 20))
@@ -46,6 +44,29 @@ class Ui_MainWindow(object):
         self.label_6.setGeometry(QtCore.QRect(510, 40, 67, 17))
         self.label_6.setText("")
         self.label_6.setObjectName("label_6")
+        self.label_8 = QtWidgets.QLabel(self.centralwidget)
+        self.label_8.setGeometry(QtCore.QRect(20, 510, 160, 25))
+        self.label_8.setObjectName("label_4")
+        self.label_9 = QtWidgets.QLabel(self.centralwidget)
+        self.label_9.setGeometry(QtCore.QRect(20, 600, 160, 25))
+        self.label_9.setObjectName("label_5")
+        self.label_10 = QtWidgets.QLabel(self.centralwidget)
+        self.label_10.setGeometry(QtCore.QRect(20, 630, 160, 25))
+        self.label_10.setObjectName("label_6")
+        self.label_11 = QtWidgets.QLabel(self.centralwidget)
+        self.label_11.setGeometry(QtCore.QRect(20, 540, 160, 25))
+        self.label_11.setObjectName("label_9")
+        self.label_12 = QtWidgets.QLabel(self.centralwidget)
+        self.label_12.setGeometry(QtCore.QRect(20, 570, 160, 25))
+        self.label_12.setObjectName("label_9")
+        self.label_13 = QtWidgets.QLabel(self.centralwidget)
+        self.label_13.setGeometry(QtCore.QRect(500, 510, 221, 151))
+        self.label_13.setText("")
+        self.label_13.setPixmap(QtGui.QPixmap("../Robotica/Laboratorio_3/Imagenes/images.png"))
+        self.label_13.setObjectName("label_13")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(340, 160, 89, 25))
+        self.pushButton.setObjectName("pushButton")
         MainWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
@@ -54,109 +75,99 @@ class Ui_MainWindow(object):
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-        self.textEdit.textChanged.connect(self.robot)
-        self.textEdit_2.textChanged.connect(self.robot)
+        # Crear lienzo para el gráfico tridimensional
+        self.fig = Figure()
+        self.fig1 = self.fig.add_subplot(projection='3d')
+        self.fig1.set_xlabel('X')
+        self.fig1.set_ylabel('Y')
+        self.fig1.set_zlabel('Z')
+        self.fig1.set_xlim(-30, 30)
+        self.fig1.set_ylim(-30, 30)
+        self.fig1.set_zlim(-30, 30)
+
+        self.canvas = FigureCanvas(self.fig)
+        layout = QtWidgets.QVBoxLayout(self.label_7)
+        layout.addWidget(self.canvas)
+        
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "Punto 2"))
         self.label.setText(_translate("MainWindow", "X"))
         self.label_2.setText(_translate("MainWindow", "Y"))
         self.label_3.setText(_translate("MainWindow", "Articulación 1"))
         self.label_4.setText(_translate("MainWindow", "Articulación 2"))
+        self.label_8.setText(_translate("MainWindow", "Juan Sebastian Torres"))
+        self.label_9.setText(_translate("MainWindow", "Juan Camilo Alberto"))
+        self.label_10.setText(_translate("MainWindow", "Sergio Andres Lopez"))
+        self.label_11.setText(_translate("MainWindow", "Steven Santana"))
+        self.label_12.setText(_translate("MainWindow", "Karen Mancilla"))
+        self.pushButton.setText(_translate("MainWindow", "Iniciar"))
+
+        self.pushButton.clicked.connect(self.robot)
 
     def robot(self):
         l1 = 6
         l2 = 8
+
+        n = 181
+        d = np.zeros((3,n))
+
         # Cinemática inversa
         x = self.textEdit.toPlainText()
         y = self.textEdit_2.toPlainText()
-        
-        if x == '' or y == '' or x == '-' or y == '-' :
-            x = 14
-            y = 0
-            
-        Px = int(x)
-        Py = int(y)
 
-        b = np.sqrt(Px ** 2 + Py ** 2)
-        # Theta 2
-        cos_theta2 = (b ** 2 - l2 ** 2 - l1 ** 2) / (2 * l1 * l2)
-        sen_theta2 = np.sqrt(1 - (cos_theta2) ** 2)
-        theta2 = np.arctan2(sen_theta2, cos_theta2)
+        # R = []
+        # R.append(RevoluteDH(d=0, alpha=0, a=l1, offset=0))
+        # R.append(RevoluteDH(d=0, alpha=0, a=l2, offset=0))
 
-        # Theta 1
-        alpha = np.arctan2(Py, Px)
-        phi = np.arctan2(l2 * sen_theta2, l1 + l2 * cos_theta2)
-        theta1 = alpha - phi
+        # Robot = DHRobot(R, name='Bender')
 
-        q1 = theta1
-        self.label_6.setText(str(np.rad2deg(q1)))
-        q2 = theta2
-        self.label_5.setText(str(np.rad2deg(q2)))
-        
-        if np.isnan(q1) or np.isnan(q2):
-            q1 = 0
-            q2 = 0
+        if x == '' or y == '':
+            for i in range(0, n, 20):
+                q1 = np.deg2rad(0)
+                q2 = np.deg2rad(i)
+                MTH = Robot.fkine([q1, q2])
+                d[:, i] = MTH.t
+                self.plot_path1(d[0, i], d[1, i])
+                self.plot_robot(Robot, q1, q2)
+                QtWidgets.qApp.processEvents()
+                sleep(0.05)
 
-        R = []
-        R.append(RevoluteDH(d=0, alpha=0, a=l1, offset=0))
-        R.append(RevoluteDH(d=0, alpha=0, a=l2, offset=0))
-
-        Robot = DHRobot(R, name='Bender')
-
-        # self.selected_motor = 33
-        # self.selected_motor = 35
-        self.move_robot(q1, q2)
-        self.plot_robot(Robot, q1, q2)
+        if x != '' and y != '':
+            Px = int(x)
+            Py = int(y)
+            b = math.sqrt(Px**2 + Py**2)
+            cos_theta2 = (b**2 - l2**2 - l1**2) / (2 * l1 * l2)
+            sen_theta2 = math.sqrt(1 - (cos_theta2)**2)
+            theta2 = math.atan2(sen_theta2, cos_theta2)
+            alpha = math.atan2(Py, Px)
+            phi = math.atan2(l2 * sen_theta2, l1 + l2 * cos_theta2)
+            theta1 = alpha - phi
+            if theta1 <= -np.pi:
+                theta1 = (2 * np.pi) + theta1
+            q1 = theta1
+            q2 = theta2
+            if np.isnan(q1) or np.isnan(q2):
+                q1 = 0
+                q2 = 0
+            if q2 <= -np.pi:
+                q2 = (2 * np.pi) + q2
+            self.plot_path1(Px, Py)
+            self.plot_robot(Robot, q1, q2)  
 
     def plot_robot(self, robot, q1, q2):
-
-        fig = Figure()
-        ax = fig.add_subplot(111, projection='3d')
         robot.plot([q1, q2], backend='pyplot', limits=[-20, 20, -20, 20, -20, 20])
-        ax.set_xlim([-20, 20])
-        ax.set_ylim([-20, 20])
-        ax.set_zlim([-20, 20])
 
-        canvas = FigureCanvas(fig)
-        layout = QtWidgets.QVBoxLayout(self.label_7)
-        layout.addWidget(canvas)
-
-
-    def move_robot(self, q1, q2):
-        
-        q1s = int(np.rad2deg(q1))
-        q2s = int(np.rad2deg(q2))
-
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(33, GPIO.OUT)
-        pulso_q1 = GPIO.PWM(33, 50)
-        pulso_q1.start(1.5)
-        grados_q1 = ((1.0/18.0) * q1s) + 2.5
-        pulso_q1.ChangeDutyCycle(grados_q1)
-        sleep(0.1)
-        pulso_q1.stop()
-        
-        GPIO.setup(35, GPIO.OUT)
-        pulso_q2 = GPIO.PWM(35, 50)
-        pulso_q2.start(1.5)
-        grados_q2 = ((1.0/18.0) * q2s) + 2.5
-        pulso_q2.ChangeDutyCycle(grados_q2)
-        sleep(0.1)
-        pulso_q2.stop()
-        GPIO.cleanup()
-
+    def plot_path1(self, x, y):
+        self.fig1.plot([x], [y], [0], '.b')
+        self.canvas.draw()
 
 if __name__ == "__main__":
     import sys
-
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
-
-
-
