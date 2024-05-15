32902813b81021e8e85d575a34f9ad16d1b66739
Ju4nS3b4st14n <126536935+Ju4nS3b4st14n@users.noreply.github.com>
2024-05-15 15:01:14 -0500

    Update Ejemplo.py

--- Ejemplo.py
+++ Ejemplo.py
@@ -1,18 +1,381 @@
-# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
-# SPDX-License-Identifier: MIT
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtGui import QPixmap, QImage
+from PyQt5.QtCore import QTimer
+from roboticstoolbox import *
+import numpy as np
+import cv2  # Importar OpenCV
+import math
+import matplotlib
+matplotlib.use('Qt5Agg')
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+import RPi.GPIO as GPIO
+import pigpio
+from time import sleep
 
-"""Simple test for a standard servo on channel 0 and a continuous rotation servo on channel 1."""
-import time
-from adafruit_servokit import ServoKit
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(769, 750)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.textEdit = QtWidgets.QTextEdit(self.centralwidget)
+        self.textEdit.setGeometry(QtCore.QRect(220, 40, 104, 25))
+        self.textEdit.setObjectName("textEdit")
+        self.textEdit_2 = QtWidgets.QTextEdit(self.centralwidget)
+        self.textEdit_2.setGeometry(QtCore.QRect(220, 70, 104, 25))
+        self.textEdit_2.setObjectName("textEdit_2")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(180, 40, 21, 25))
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(180, 70, 21, 25))
+        self.label_2.setObjectName("label_2")
+        self.label_7 = QtWidgets.QWidget(self.centralwidget)
+        self.label_7.setGeometry(QtCore.QRect(100, 100, 551, 371))
+        self.label_7.setObjectName("label_7")
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(396, 40, 101, 20))
+        self.label_3.setObjectName("label_3")
+        self.label_4 = QtWidgets.QLabel(self.centralwidget)
+        self.label_4.setGeometry(QtCore.QRect(396, 70, 101, 20))
+        self.label_4.setObjectName("label_4")
+        self.label_5 = QtWidgets.QLabel(self.centralwidget)
+        self.label_5.setGeometry(QtCore.QRect(510, 70, 67, 17))
+        self.label_5.setText("")
+        self.label_5.setObjectName("label_5")
+        self.label_6 = QtWidgets.QLabel(self.centralwidget)
+        self.label_6.setGeometry(QtCore.QRect(510, 40, 67, 17))
+        self.label_6.setText("")
+        self.label_6.setObjectName("label_6")
+        self.label_8 = QtWidgets.QLabel(self.centralwidget)
+        self.label_8.setGeometry(QtCore.QRect(20, 510, 160, 25))
+        self.label_8.setObjectName("label_4")
+        self.label_9 = QtWidgets.QLabel(self.centralwidget)
+        self.label_9.setGeometry(QtCore.QRect(20, 600, 160, 25))
+        self.label_9.setObjectName("label_5")
+        self.label_10 = QtWidgets.QLabel(self.centralwidget)
+        self.label_10.setGeometry(QtCore.QRect(20, 630, 160, 25))
+        self.label_10.setObjectName("label_6")
+        self.label_11 = QtWidgets.QLabel(self.centralwidget)
+        self.label_11.setGeometry(QtCore.QRect(20, 540, 160, 25))
+        self.label_11.setObjectName("label_9")
+        self.label_12 = QtWidgets.QLabel(self.centralwidget)
+        self.label_12.setGeometry(QtCore.QRect(20, 570, 160, 25))
+        self.label_12.setObjectName("label_9")
+        self.label_13 = QtWidgets.QLabel(self.centralwidget)
+        self.label_13.setGeometry(QtCore.QRect(500, 510, 221, 151))
+        self.label_13.setText("")
+        self.label_13.setPixmap(QtGui.QPixmap("../Robotica/Laboratorio_3/Imagenes/images.png"))
+        self.label_13.setObjectName("label_13")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(340, 118, 89, 25))
+        self.pushButton.setObjectName("pushButton")
+        self.comboBox = QtWidgets.QComboBox(self.centralwidget)
+        self.comboBox.setGeometry(QtCore.QRect(290, 500, 191, 25))
+        self.comboBox.setObjectName("comboBox")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
 
-# Set channels to the number of servo channels on your kit.
-# 8 for FeatherWing, 16 for Shield/HAT/Bonnet.
-kit = ServoKit(channels=8)
+        self.fig = Figure()
+        self.fig1 = self.fig.add_subplot(projection='3d')
+        self.fig1.set_xlabel('X')
+        self.fig1.set_ylabel('Y')
+        self.fig1.set_zlabel('Z')
+        self.fig1.set_xlim(-10, 10)
+        self.fig1.set_ylim(0, 10)
+        self.fig1.set_zlim(0, 10)
 
-kit.servo[0].angle = 180
-kit.continuous_servo[1].throttle = 1
-time.sleep(1)
-kit.continuous_servo[1].throttle = -1
-time.sleep(1)
-kit.servo[0].angle = 0
-kit.continuous_servo[1].throttle = 0
+        self.canvas = FigureCanvas(self.fig)
+        layout = QtWidgets.QVBoxLayout(self.label_7)
+        layout.addWidget(self.canvas)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Punto 2"))
+        self.label.setText(_translate("MainWindow", "X"))
+        self.label_2.setText(_translate("MainWindow", "Y"))
+        self.label_3.setText(_translate("MainWindow", "Articulación 1"))
+        self.label_4.setText(_translate("MainWindow", "Articulación 2"))
+        self.label_8.setText(_translate("MainWindow", "Juan Sebastian Torres"))
+        self.label_9.setText(_translate("MainWindow", "Juan Camilo Alberto"))
+        self.label_10.setText(_translate("MainWindow", "Sergio Andres Lopez"))
+        self.label_11.setText(_translate("MainWindow", "Steven Santana"))
+        self.label_12.setText(_translate("MainWindow", "Karen Mancilla"))
+        self.pushButton.setText(_translate("MainWindow", "Iniciar"))
+        self.comboBox.addItem("Selecciona un logo")
+        self.comboBox.addItem("Chevrolet")
+        self.comboBox.addItem("Renault")
+        self.comboBox.addItem("Mercedes")
+        self.comboBox.addItem("Kia")
+        
+
+        self.pushButton.clicked.connect(self.robot)
+        self.comboBox.currentTextChanged.connect(self.cars)
+
+    def robot(self):
+
+        l1 = 6
+        l2 = 8
+
+        n = 181
+        d = np.zeros((3,n))
+
+        # Cinemática inversa
+        x = self.textEdit.toPlainText()
+        y = self.textEdit_2.toPlainText()
+
+        R = []
+        R.append(RevoluteDH(d=0, alpha=0, a=l1, offset=0))
+        R.append(RevoluteDH(d=0, alpha=0, a=l2, offset=0))
+
+        Robot = DHRobot(R, name='Bender')
+
+        if x == '' or y == '':
+
+            for i in range(0, n, 20):
+                q1 = np.deg2rad(0)
+                q2 = np.deg2rad(i)
+
+                MTH = Robot.fkine([q1,q2])
+                d[:,i] =  MTH.t 
+                self.move_robot(q1, q2)
+                self.plot_path(d, i)
+                self.plot_robot(Robot, q1, q2)
+                sleep(0.05)
+            
+            for i in range(n, 0, -20):
+                q1 = np.deg2rad(0)
+                q2 = np.deg2rad(i)
+
+                self.move_robot(q1, q2)
+                self.plot_robot(Robot, q1, q2)
+                sleep(0.05)
+
+            for i in range(0, n, 20):
+                q1 = np.deg2rad(i)
+                q2 = np.deg2rad(0)
+                
+                MTH = Robot.fkine([q1,q2])
+                d[:,i] =  MTH.t 
+                self.move_robot(q1, q2)
+                self.plot_path(d, i)
+                self.plot_robot(Robot, q1, q2)
+                sleep(0.05)
+
+            for i in range(0, n, 20):
+                q1 = np.deg2rad(180)
+                q2 = np.deg2rad(i)
+
+                MTH = Robot.fkine([q1,q2])
+                d[:,i] =  MTH.t 
+                self.move_robot(q1, q2)
+                self.plot_path(d, i)
+                self.plot_robot(Robot, q1, q2)
+                sleep(0.05)
+
+        if x != '' and y != '':
+
+            Px = int(x)
+            Py = int(y)
+
+            b = math.sqrt(Px**2+Py**2)
+            # Theta 2
+            cos_theta2 = (b**2-l2**2-l1**2)/(2*l1*l2)
+            sen_theta2 = math.sqrt(1-(cos_theta2)**2)#(+)codo abajo y (-)codo arriba
+            theta2 = math.atan2(sen_theta2, cos_theta2)
+            # Theta 1
+            alpha = math.atan2(Py,Px)
+            phi = math.atan2(l2*sen_theta2, l1+l2*cos_theta2)
+            theta1 = alpha - phi
+
+            if theta1 <= -np.pi:
+                theta1 = (2*np.pi)+theta1        
+
+            q1 = theta1
+            q2 = theta2
+
+            if q2 <= -np.pi:
+                q2 = (2*np.pi)+q2
+                
+            if np.isnan(q1) or np.isnan(q2):
+                q1 = 0
+                q2 = 0
+
+            self.move_robot(q1, q2)
+            self.plot_path1(Px, Py)
+            self.plot_robot(Robot, q1, q2)  
+
+    def cars(self, seleccion):
+
+        # Define las rutas de las imágenes según la opción seleccionada
+        if seleccion == "Chevrolet":
+            # Leer la imagen con OpenCV
+            img = cv2.imread('../Robotica/Laboratorio_3/Imagenes/Chevrolet.jpg')
+        elif seleccion == "Renault":
+            img = cv2.imread('../Robotica/Laboratorio_3/Imagenes/Renault.png')
+        elif seleccion == "Mercedes":
+            img = cv2.imread('../Robotica/Laboratorio_3/Imagenes/Mercedes.png')
+        elif seleccion == "Kia":
+            img = cv2.imread('../Robotica/Laboratorio_3/Imagenes/Kia2.jpg')
+
+        # Convertir la imagen a escala de grises
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)     
+
+        # Obtener contornos
+        _, binaria = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+        contornos, _ = cv2.findContours(binaria, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+        coordenadas_x = []
+        coordenadas_y = []
+        
+        # Imprimir las coordenadas de los contornos
+        for contour in contornos:
+            for punto in contour:
+                x, y = punto[0]
+                coordenadas_x.append(x)
+                coordenadas_y.append(y)
+
+        self.robot_cars(coordenadas_x, coordenadas_y, seleccion)
+
+    def robot_cars(self, coordenadas_x, coordenadas_y, seleccion):
+        # Restringir el número de coordenadas a procesar
+        if seleccion == "Chevrolet":
+            coordenadas_x = coordenadas_x[:94]
+            coordenadas_y = coordenadas_y[:94]
+
+        elif seleccion == "Renault":
+            coordenadas_x = coordenadas_x[:710]
+            coordenadas_y = coordenadas_y[:710]
+
+        elif seleccion == "Mercedes":
+            coordenadas_x = coordenadas_x[:820]
+            coordenadas_y = coordenadas_y[:820]
+
+        elif seleccion == "Kia":
+            coordenadas_x = coordenadas_x[:446]
+            coordenadas_y = coordenadas_y[:446]
+
+        l1 = 6
+        l2 = 8
+
+        d = np.zeros((3, len(coordenadas_x)))
+
+        R = []
+        R.append(RevoluteDH(d=0, alpha=0, a=l1, offset=0))
+        R.append(RevoluteDH(d=0, alpha=0, a=l2, offset=0))
+        Robot = DHRobot(R, name='Bender')
+
+        for i in range(len(coordenadas_x)):
+            x, y = coordenadas_x[i], coordenadas_y[i]
+            # Cinemática inversa
+            if seleccion == "Chevrolet":
+                Px = x/70-5
+                Py = y/70+6
+            elif seleccion == "Renault":
+                Px = x/50-2
+                Py = y/50+6
+            elif seleccion == "Mercedes":
+                Px = x/60-2
+                Py = y/60+6
+            elif seleccion == "Kia":
+                Px = x/70-4
+                Py = y/70+6
+
+            b = math.sqrt(Px**2+Py**2)
+            # Theta 2
+            cos_theta2 = (b**2-l2**2-l1**2)/(2*l1*l2)
+            sen_theta2 = math.sqrt(1-(cos_theta2)**2)#(+)codo abajo y (-)codo arriba
+            theta2 = math.atan2(sen_theta2, cos_theta2)
+        
+            # Theta 1
+            alpha = math.atan2(Py,Px)
+            phi = math.atan2(l2*sen_theta2, l1+l2*cos_theta2)
+            theta1 = alpha - phi
+        
+            if theta1 <= -np.pi:
+                theta1 = (2*np.pi)+theta1 
+
+            q1 = theta1
+            q2 = theta2
+
+            if q2 <= -np.pi:
+                q2 = (2*np.pi)+q2
+
+            MTH = Robot.fkine([q1,q2])
+            d[:, i] =  MTH.t 
+
+            self.plot_path4(d, i)
+            #self.move_robot(self, q1, q2)
+            #self.plot_robot(Robot, q1, q2)
+
+
+    def plot_robot(self, robot, q1, q2):
+        
+        self.label_5.setText(str(np.rad2deg(q2)))
+        self.label_6.setText(str(np.rad2deg(q1)))
+        robot.plot([q1, q2], backend='pyplot', limits=[-20, 20, -20, 20, -20, 20])
+
+    def plot_path(self, d, i):
+
+        self.fig1.plot(d[0,i],d[1,i],d[2,i],'.b')
+        self.canvas.draw()
+
+    def plot_path1(self, x, y):
+
+        self.fig1.plot(x, y, 0,'.b')
+        self.canvas.draw()
+
+    def plot_path4(self, d, i):
+
+        if i > 0:
+            self.fig1.plot([d[0, i-1], d[0, i]], [d[1, i-1], d[1, i]], [d[2, i-1], d[2, i]], color='blue')
+        self.canvas.draw()
+        
+    def move_robot(self, q1, q2):
+        
+        q1s = int(np.rad2deg(q1))
+        q2s = int(np.rad2deg(q2))
+
+        # Inicializar el cliente pigpio
+        pi = pigpio.pi()
+
+        # Configurar los pines GPIO para los servomotores
+        GPIO_PIN_Q1 = 33
+        GPIO_PIN_Q2 = 35
+        pi.set_mode(GPIO_PIN_Q1, pigpio.OUTPUT)
+        pi.set_mode(GPIO_PIN_Q2, pigpio.OUTPUT)
+
+        # Calcular el ancho de pulso para cada servo
+        pulso_q1 = ((1.0/18.0) * q1s) + 2.5
+        pulso_q2 = ((1.0/18.0) * q2s) + 2.5
+
+        # Generar señales PWM simultáneas y sincronizadas para los servos
+        pi.set_servo_pulsewidth(GPIO_PIN_Q1, pulso_q1)
+        pi.set_servo_pulsewidth(GPIO_PIN_Q2, pulso_q2)
+
+        # Esperar un tiempo suficiente para que los servomotores se muevan
+        sleep(0.1)
+
+        # Detener las señales PWM y limpiar
+        pi.set_servo_pulsewidth(GPIO_PIN_Q1, 0)
+        pi.set_servo_pulsewidth(GPIO_PIN_Q2, 0)
+
+        # Cerrar la conexión con pigpio
+        pi.stop()
+
+if __name__ == "__main__":
+    import sys
+
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
