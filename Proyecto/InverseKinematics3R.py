ea3fd4ce93ae186be8d90715efcf56789ccd0e8b
Ju4nS3b4st14n <126536935+Ju4nS3b4st14n@users.noreply.github.com>
2024-05-23 09:25:47 -0500

    Update InverseKinematics3R.py

--- Proyecto/InverseKinematics3R.py
+++ Proyecto/InverseKinematics3R.py
@@ -10,18 +10,21 @@ def InverseKinematics3R(l1,l2,l3,Px,Py,Pz):
     b = sqrt(e**2+c**2)
     # Theta 1
     theta1 = float(atan2(Py,Px))
+    if numpy.isnan(theta1) :
+            theta1 = 0
     print(f'theta 1 = {numpy.rad2deg(theta1):.4f}')
     # Theta 3
     cos_theta3 = (b**2-l2**2-l3**2)/(2*l2*l3)
-    sen_theta3 = sqrt(1-(cos_theta3)**2)
+    sen_theta3 = -sqrt(1-(cos_theta3)**2)#(+)codo abajo y (-)codo arriba
     theta3 = float(atan2(sen_theta3, cos_theta3))
+    theta3 = theta3+numpy.pi/2
     print(f'theta 3 = {numpy.rad2deg(theta3):.4f}')
     # Theta 2
     alpha = math.atan2(c,e)
     phi = math.atan2(l3*sen_theta3, l2+l3*cos_theta3)
     theta2 = float(alpha - phi)
-    if theta2 <= -pi:
-        theta2 = (2*pi)+theta2
+    if theta2 <= -numpy.pi:
+        theta2 = (2*numpy.pi)+theta2
 
     print(f'theta 2 = {numpy.rad2deg(theta2):.4f}')
     
